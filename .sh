06cb2f65a0353e70f0a38cbcbd6b857fddc3d17f
ChristosKyriazis <101571234+ChristosKyriazis@users.noreply.github.com>
2022-05-23 13:26:01 -0400

    Created basic installer script

--- .sh
+++ .sh
@@ -1,3 +1,54 @@
 #!/usr/bin/env sh
 
-echo "Test"
+Release="https://github.com/ServedSpicy/Bundle/releases/download/Dummy/Dummy.zip"
+Folder="$HOME/.ServedSpicy"
+Download="${Folder}/Download.zip"
+Applications="$HOME/.local/share/applications"
+DesktopEntry="${Applications}/ServedSpicy-Configurator.desktop"
+
+
+# Install Deno
+
+echo "Installing Deno"
+
+curl -fsSL https://deno.land/install.sh | sh > /dev/null
+
+
+# Install Serial Library
+
+echo "Installing Serial Library"
+
+sudo apt install libserial1
+
+
+# Setting Up Files
+
+rm              \
+    --recursive \
+    $Folder
+    
+mkdir $Folder
+
+
+# Download Release
+
+echo "Downloading Latest Release"
+
+curl                    \
+    --output $Download  \
+    -LJO                \
+    $Release
+    
+    
+# Extract Archive
+
+unzip           \
+    -d $Folder  \
+    $Download
+
+
+# Cleanup
+
+rm $Download
+
+mv $Folder/Dummy/ $Folder/Configurator/
