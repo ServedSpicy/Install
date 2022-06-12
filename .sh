664aa1e16dcf62d09dc79e27d33866ddef99b107
ChristosKyriazis <101571234+ChristosKyriazis@users.noreply.github.com>
2022-06-12 14:27:24 -0400

    New installer approach

--- .sh
+++ .sh
@@ -1,66 +1,26 @@
 #!/usr/bin/env sh
 
+name="ServedSpicy Installer"
+file_temp=/tmp/ServedSpicy_installer_XXXXXX.sh
+link_installer=https://raw.githubusercontent.com/ServedSpicy/Install/main/Installer.sh
+
+
 clear
 
 
-# Ensure Deno
+installer=$(mktemp $file_temp)
 
-if ! type "deno" > /dev/null; then
+curl -sfS $link_installer > $installer
+
+sudo chmod +x $installer
+
+sudo gnome-terminal \
+    --hide-menubar  \
+    --title $name   \
+    --name=$name    \
+    -- $installer
     
-    curl -fsSL https://deno.land/install.sh | sh > /dev/null
-    
-    echo 'export DENO_INSTALL="/$HOME/.deno"' >> $HOME/.bashrc
-    echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> $HOME/.bashrc
-    
-    source ~/.bashrc
-fi
+rm $installer
 
-
-# Use the local installer in debug mode.
-
-if [ $1 = "debug" ]; then
-    
-    echo "Debug Mode Enabled"
-    
-    current="$(dirname -- $(readlink -fn -- "$0"; echo x))/"
-    
-    installer="${current}/../Installer/Source/App.js"
-    imports="${current}/../Installer/Source/Imports.json"
-    
-else
-    
-    installer="https://raw.githubusercontent.com/ServedSpicy/Installer/0.3.2/Source/App.js"
-    imports="https://raw.githubusercontent.com/ServedSpicy/Installer/0.3.2/Source/Imports.json"
-
-fi
-
-
-deno="$HOME/.deno/bin/deno"
-
-desktop_entry="$HOME/.local/share/applications/ServedSpicy.desktop"
-folder="$HOME/.ServedSpicy"
-config="$HOME/.config/ServedSpicy"
-temp="/tmp"
-
-writable="${desktop_entry},${folder},${config},${temp}"
-readable="${folder},${config},${temp}"
-commands="dpkg-query,apt,curl,unzip,groups,usermod"
-variables="HOME"
-
-
-user=$(id -u $USER)
-group=$(id -g $USER)
 
 clear
-
-sudo $deno run                  \
-    --allow-write=$writable     \
-    --allow-read=$readable      \
-    --allow-env=$variables      \
-    --importmap=$imports        \
-    --allow-run=$commands       \
-    --unstable                  \
-    $installer                  \
-    --home=$HOME                \
-    --user=$user                \
-    --group=$group
---
+++ Installer.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env sh
+
+clear
+
+
+# Ensure Deno
+
+if ! type "deno" > /dev/null; then
+    
+    curl -fsSL https://deno.land/install.sh | sh > /dev/null
+    
+    echo 'export DENO_INSTALL="/$HOME/.deno"' >> $HOME/.bashrc
+    echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> $HOME/.bashrc
+    
+    source ~/.bashrc
+else
+    
+    echo "Deno is already installed"
+    
+fi
+
+
+# Use the local installer in debug mode.
+
+if [ $1 = "debug" ]; then
+    
+    echo "Debug Mode Enabled"
+    
+    current="$(dirname -- $(readlink -fn -- "$0"; echo x))/"
+    
+    installer="${current}/../Installer/Source/App.js"
+    imports="${current}/../Installer/Source/Imports.json"
+    
+else
+    
+    installer="https://raw.githubusercontent.com/ServedSpicy/Installer/0.3.2/Source/App.js"
+    imports="https://raw.githubusercontent.com/ServedSpicy/Installer/0.3.2/Source/Imports.json"
+
+fi
+
+
+deno="$HOME/.deno/bin/deno"
+
+desktop_entry="$HOME/.local/share/applications/ServedSpicy.desktop"
+folder="$HOME/.ServedSpicy"
+config="$HOME/.config/ServedSpicy"
+temp="/tmp"
+
+writable="${desktop_entry},${folder},${config},${temp}"
+readable="${folder},${config},${temp}"
+commands="dpkg-query,apt,curl,unzip,groups,usermod"
+variables="HOME"
+
+
+user=$(id -u $USER)
+group=$(id -g $USER)
+
+# clear
+
+sudo $deno run                  \
+    --allow-write=$writable     \
+    --allow-read=$readable      \
+    --allow-env=$variables      \
+    --importmap=$imports        \
+    --allow-run=$commands       \
+    --unstable                  \
+    $installer                  \
+    --home=$HOME                \
+    --user=$user                \
+    --group=$group
